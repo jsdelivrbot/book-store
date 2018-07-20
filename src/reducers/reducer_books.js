a960ee13239ecd3a55f259f0e091702ee9b7ed1f
Abdelkarim <kimoacm@gmail.com>
2018-07-20 20:07:38 +0200

    first commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+  return[
+    { title: 'javascipt: the good parts' },
+    { title: 'harry potter' },
+    { title: 'the dark tower' },
+    { title: 'Eloquent Ruby' }
+  ]
+}
